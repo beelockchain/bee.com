ec80d226e92ff176157d9417f06a4cf784b56449
kaleeswari <kaleeswari@sportstech.de>
2026-02-13 10:03:24 +0530

    digital transmission subpage

---
+++ app/digital-transforms/page.js
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import BusinessValue from './components/BusinessValue'
+import RoadMapCard from './components/RoadMapCard'
+
+export const metadata = {
+  title: ' Digital Transforms | Innovation & Technology',
+  description:
+    'Explore leverages digital transformations to enhance fitness technology, user experience, and data security.',
+};
+
+
+export default function DigitalTransforms() {
+  
+  return (
+      <main>
+         <BusinessValue />
+         <RoadMapCard />
+      </main>
+
+
+  )
+}
