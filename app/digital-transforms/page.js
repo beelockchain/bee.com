7925ff36919c7142c73bb138e1d8a40cf3decb49
vigneshraja122 <vraja3868@gmail.com>
2026-02-16 11:55:56 +0530

    Added 4 section in subpage.

--- app/digital-transforms/page.js
+++ app/digital-transforms/page.js
@@ -1,28 +1,28 @@
-
-import React from 'react'
-import BusinessValue from './components/BusinessValue'
-import RoadMapCard from './components/RoadMapCard'
-import DigitalTransformationSection from './components/DigitalTransformation' ;
-import Whatwedo from './components/Whatwedo'
+import React from "react";
+import BusinessValue from "./components/BusinessValue";
+import RoadMapCard from "./components/RoadMapCard";
+import DigitalTransformationSection from "./components/DigitalTransformation";
+import Whatwedo from "./components/Whatwedo";
+import WhyDigitalTransformation from "./components/Whydigitaltrans";
+import FAQ from "./components/FAQ";
+import SubIndustriesWeServe from "./components/Sub-industriesweserve";
 
 export const metadata = {
-  title: ' Digital Transforms | Innovation & Technology',
+  title: " Digital Transforms | Innovation & Technology",
   description:
-    'Explore leverages digital transformations to enhance fitness technology, user experience, and data security.',
+    "Explore leverages digital transformations to enhance fitness technology, user experience, and data security.",
 };
 
-
 export default function DigitalTransforms() {
-  
   return (
-      <main>
-         <BusinessValue />
-         <RoadMapCard />
-         <DigitalTransformationSection/>
-         <Whatwedo/>
-
-      </main>
-
-
-  )
+    <main>
+      <BusinessValue />
+      <RoadMapCard />
+      <DigitalTransformationSection />
+      <Whatwedo />
+      <WhyDigitalTransformation />
+      <SubIndustriesWeServe />
+      <FAQ />
+    </main>
+  );
 }
